5758c9d82bd5322c25ec7d007682bfd7506977f2
dev-9820 <dev.bhanushali03@gmail.com>
2024-08-25 13:51:45 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ cors = CORS(app, resources={r'/orders':{'origins': '*'}})
 client = MongoClient("mongodb+srv://db_user_read:LdmrVA5EDEv4z3Wr@cluster0.n10ox.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 db = client.RQ_Analytics
 
-@app.route('/', methods=['GET'])
+@app.route('/orders', methods=['GET'])
 def get_data():
     data = list(db.shopifyOrders.find({}, {'_id': 0}))
     return jsonify(data)
